b2125d7d2f4600f5c3c819a9be81c8ef1b2d354b
Alpeacha <115733768+Alpeacha@users.noreply.github.com>
2022-10-20 21:25:51 +0530

    Add files via upload

---
+++ src/jsonData/InvoiceData.js
@@ -0,0 +1,32 @@
+const Invoicedata = {
+    "id": "63515c06e0f0dd0b094c710c",
+    "invoice_no": "202208-65",
+    "balance": "1:05:05",
+    "company": "ESHKON",
+    "email": "reneetrujillo@eshkon.com",
+    "phone": "+91 123456789",
+    "address": " 90 , XYZ Place, Surat, Gujrat , Pincode",
+    "trans_date": "14-01-2022",
+    "due_date": "12-10-2022",
+    "items": [
+      {
+        "sno": 1,
+        "desc": "item 1",
+        "qty": 9,
+        "rate": 394.13
+      },
+      {
+        "sno": 2,
+        "desc": "item 2",
+        "qty": 7,
+        "rate": 849.35
+      },
+      {
+        "sno": 3,
+        "desc": "item 3",
+        "qty": 6,
+        "rate": 788.34
+      }
+    ]
+  }
+  export default Invoicedata;
